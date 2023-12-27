f646283b9bf448b8953f3f5a1a31d0b35e5c0825
AhmVortex <ahmedabdelrahim228@gmail.com>
2023-12-27 04:56:36 +0200

    ahmed new

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:logistics/LoginScreen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'main.dart';
+import 'services.dart';
 
 final List<String> imageUrls = [
   'assets/icon1.jpeg',
@@ -11,6 +13,25 @@ final List<String> imageUrls = [
   'assets/portrait3.jpeg',
 ];
 
+final List<String> textTitle = [
+  'Normal Transportation',
+  'Packaging',
+  'Scheduling',
+  'Insured Transportation',
+  'TakeCare',
+  'Wrapper'
+];
+
+final List<String> Description = [
+  'Transport your goods in safety without any concerns about the damage of your goods',
+  'A special option for those who want their goods handled with extra care',
+  'Additional wrapping options to protect fragile or valuable items'
+];
+
+final List<Widget> _pages = [
+  HomePage(),
+  ServicesPage(),
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key});
@@ -20,7 +41,10 @@ class HomePage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Home'),
+          title: const Text(
+            'SPLT',
+            style: TextStyle(color: Colors.black),
+          ),
           backgroundColor: Colors.orange,
           actions: [
             IconButton(
@@ -30,124 +54,204 @@ class HomePage extends StatelessWidget {
                 await loginScreenState.clearUserData();
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()), // Replace LoginScreen with your actual login screen widget
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          LoginScreen()), // Replace LoginScreen with your actual login screen widget
                 );
               },
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  // Open Google Maps
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: const Row(
-                    children: [
-                      Icon(Icons.search, color: Colors.orange),
-                      SizedBox(width: 10.0),
-                      Text('Where to?'),
-                    ],
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    // Open Google Maps
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(Icons.search, color: Colors.orange),
+                        SizedBox(width: 10.0),
+                        Text('Where ttto?'),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  // Open Google Maps
-                },
-                child: Container(
+                const SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    // Open Google Maps
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: const Row(
+                      children: [
+                        Icon(Icons.location_on, color: Colors.orange),
+                        SizedBox(width: 10.0),
+                        Text('Previous Location'),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                //SUGGESSTIONS
+                Container(
                   width: double.infinity,
-                  height: 40.0,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: const Row(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Icon(Icons.location_on, color: Colors.orange),
-                      SizedBox(width: 10.0),
-                      Text('Previous Location'),
+                      Row(children: [
+                        Text(
+                          'Suggestions',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange,
+                          ),
+                        ),
+                        SizedBox(width: 150),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ServicesPage()),
+                            );
+                          },
+                          child: Text(
+                            'View All',
+                            style: TextStyle(
+                              fontSize: 13.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ]),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        height: 120.0,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: 4,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                width: 100.0,
+                                margin: EdgeInsets.only(right: 10.0),
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      imageUrls[index],
+                                      height: 80.0,
+                                    ),
+                                    SizedBox(height: 5.0),
+                                    Text(
+                                      textTitle[index],
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              );
+                            }),
+                      )
                     ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                'Suggestions',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Container(
-                height: 100,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: imageUrls.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            imageUrls[index],
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
-                          ),
-                          Text('Icon ${index + 1}'),
-                        ],
+                const SizedBox(height: 16),
+
+                //MORE WAYS TO USE
+
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'More ways to use SPLT',
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange),
                       ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'More ways to use SPLT',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Container(
-                height: 150,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: imageUrls.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            imageUrls[index],
-                            width: 120,
-                            height: 120,
-                            fit: BoxFit.cover,
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            'Title ${index + 1}',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          const Text('Description'),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
+                      SizedBox(height: 10.0),
+
+                      //pictures, titles, descriptions scrollable horizentaly
+                      Container(
+                        height: 200.0,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: 3,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                width: 150.0,
+                                margin: EdgeInsets.only(right: 10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    //container specific for image
+                                    Container(
+                                      width: double.infinity,
+                                      height: 100.0,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image:
+                                              AssetImage(imageUrls[index + 3]),
+                                          fit: BoxFit.cover,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                      ),
+                                    ),
+                                    SizedBox(height: 5.0),
+                                    Text(
+                                      textTitle[index + 3],
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 4.0,
+                                    ),
+                                    Text(
+                                      Description[index],
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                            }),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
