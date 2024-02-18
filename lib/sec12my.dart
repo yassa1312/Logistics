5e9d7d20817a512daca1b5c05beaa519d46f0565
Yassa Ashraf <139929196+yassa1312@users.noreply.github.com>
2024-02-18 02:09:16 +0200

    some acount fix

--- lib/sec12my.dart
+++ lib/sec12my.dart
@@ -5,81 +5,117 @@ void main() {
     home: HomeScreen(),
   ));
 }
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   HomeScreenState createState() => HomeScreenState();
 }
-class HomeScreenState extends State<HomeScreen> {
-  List<String> notes = [];
-  List<String> deletedNotes = [];
 
+class NoteLine {
+  final String text1;
+  final TextStyle style1;
+  final String text2;
+  final TextStyle style2;
+  final IconData? iconData;
+  final TextStyle iconStyle;
+
+  NoteLine({
+    required this.text1,
+    required this.style1,
+    required this.text2,
+    required this.style2,
+    this.iconData,
+    required this.iconStyle,
+  });
+}
+
+List<List<NoteLine>> notes = [
+  [
+    NoteLine(
+      text1: "Laurent 20:18",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "How about meeting tomorrow?",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  [
+    NoteLine(
+      text1: "Tracy 19:22",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "I love that idea, it's great!",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  [
+    NoteLine(
+      text1: "Claire 14:34",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "I wasn't aware of that,\nLet me check",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  [
+    NoteLine(
+      text1: "Joe 11:05",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "Flutter just release 1.0 officially.\nShould I go for it?",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  [
+    NoteLine(
+      text1: "Mark 09:46",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "It totally makes sense to get some \n extra day-off.",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  [
+    NoteLine(
+      text1: "Williams 08:15",
+      style1: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      text2: "It has been rescheduled to next \n Saturday 7.30pm?",
+      style2: const TextStyle(fontSize: 18, color: Colors.grey),
+      iconData: Icons.arrow_forward_ios,
+      iconStyle: const TextStyle(fontSize: 16, color: Colors.grey),
+    ),
+  ],
+  // Add more notes as needed...
+];
+
+List<List<NoteLine>> deletedNotes = [];
+
+class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddNoteScreen()),
-          ).then((result) {
-            if (result != null && result is Map<String, String>) {
-              final String title = result['title'] ?? '';
-              final String content = result['content'] ?? '';
-              if (title.isNotEmpty && content.isNotEmpty) {
-                notes.add('$title: $content');
-                setState(() {});
-              }
-            }
-          });
-        },
-        child: const Icon(Icons.add),
+        title: const Center(child: Text("Home")),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ListView.builder(
-              itemCount: notes.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return buildNoteItem(index);
-              },
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RestoreNoteScreen(
-                      deletedNotes: deletedNotes,
-                      onRestore: _restoreDeletedNote,
-                    ),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.green,
-                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                elevation: 0,
+            const SizedBox(height: 20),
+            for (int index = 0; index < notes.length; index++)
+              Column(
+                children: [
+                  buildNoteItem(index),
+                  if (index < notes.length - 1)
+                    const MyDivider(),
+                ],
               ),
-              child: const Text(
-                "Restore Deleted Items",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
           ],
         ),
       ),
@@ -87,360 +123,81 @@ class HomeScreenState extends State<HomeScreen> {
   }
 
   Widget buildNoteItem(int index) {
-    final noteParts = notes[index].split(':');
-    final title = noteParts[0];
-    final content = noteParts[1];
-
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(8),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 3,
-            blurRadius: 7,
-            offset: const Offset(0, 3),
-          ),
-        ],
+        borderRadius: BorderRadius.circular(0),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: Text(
-              title,
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 24, // Larger font size for the title
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              content,
-              style: const TextStyle(
-                color: Colors.grey,
-                fontSize: 20, // Smaller font size for the content
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              const SizedBox(width: 20),
-              Expanded(
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    _deleteNoteAt(index);
-                  },
-                  icon: const Icon(Icons.delete),
-                  label: const Text("Delete"),
-                  style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    backgroundColor: Colors.red,
-                    elevation: 0,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditNoteScreen(
-                          initialTitle: title,
-                          initialContent: content,
-                        ),
-                      ),
-                    ).then((result) {
-                      if (result != null && result is Map<String, String>) {
-                        final String editedTitle = result['title'] ?? '';
-                        final String editedContent = result['content'] ?? '';
-                        if (editedTitle.isNotEmpty && editedContent.isNotEmpty) {
-                          notes[index] = '$editedTitle: $editedContent';
-                          setState(() {});
-                        }
-                      }
-                    });
-                  },
-                  icon: const Icon(Icons.edit),
-                  label: const Text("Edit"),
-                  style: ElevatedButton.styleFrom(
-                    shape: const StadiumBorder(),
-                    elevation: 0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _deleteNoteAt(int index) {
-    if (index >= 0 && index < notes.length) {
-      deletedNotes.add(notes[index]);
-      notes.removeAt(index);
-      setState(() {});
-    }
-  }
-
-  void _restoreDeletedNote(String deletedNote) {
-    deletedNotes.remove(deletedNote);
-    notes.add(deletedNote);
-    setState(() {});
-  }
-}
-class ElevatedButtonWidget extends StatelessWidget {
-  final String label;
-  final VoidCallback? onPressed;
-  final Color? backgroundColor;
-
-  const ElevatedButtonWidget({
-    Key? key,
-    required this.label,
-    this.onPressed,
-    this.backgroundColor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: backgroundColor,
-        shape: const StadiumBorder(),
-        elevation: 2,
-      ),
-      child: Text(label),
-    );
-  }
-}
-class AddNoteScreen extends StatelessWidget {
-  final TextEditingController titleController = TextEditingController();
-  final TextEditingController contentController = TextEditingController();
-
-  AddNoteScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Add Note"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: titleController,
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Title',
-                border: OutlineInputBorder(),
-              ),
-              style: const TextStyle(fontSize: 24), // Larger font size for the title
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: contentController,
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              decoration: const InputDecoration(
-                labelText: 'Note Content',
-                border: OutlineInputBorder(),
-              ),
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                final String title = titleController.text;
-                final String content = contentController.text;
-                if (title.isNotEmpty && content.isNotEmpty) {
-                  Navigator.of(context).pop({'title': title, 'content': content});
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue, // Change the background color to match the "Restore Deleted Items" button
-                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                elevation: 0,
-                minimumSize: Size(double.infinity, 0), // Set the button width to fill the available space
-              ),
-              child: const Text(
-                "Add",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-class EditNoteScreen extends StatefulWidget {
-  final String initialTitle;
-  final String initialContent;
-
-  const EditNoteScreen({
-    required this.initialTitle,
-    required this.initialContent,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  EditNoteScreenState createState() => EditNoteScreenState();
-}
-class EditNoteScreenState extends State<EditNoteScreen> {
-  late TextEditingController titleController;
-  late TextEditingController contentController;
-
-  @override
-  void initState() {
-    super.initState();
-    titleController = TextEditingController(text: widget.initialTitle);
-    contentController = TextEditingController(text: widget.initialContent);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edit Note"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: titleController,
-              textInputAction: TextInputAction.next,
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Title',
-                border: OutlineInputBorder(),
-              ),
-              style: const TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: contentController,
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.multiline,
-              maxLines: null,
-              decoration: const InputDecoration(
-                labelText: 'Note Content',
-                border: OutlineInputBorder(),
-              ),
-              style: const TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                final String title = titleController.text;
-                final String content = contentController.text;
-                if (title.isNotEmpty && content.isNotEmpty) {
-                  Navigator.of(context).pop({'title': title, 'content': content});
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue, // Change the background color to match the "Restore Deleted Items" button
-                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                elevation: 0,
-                minimumSize: Size(double.infinity, 0), // Set the button width to fill the available space
-              ),
-              child: const Text(
-                "update",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    titleController.dispose();
-    contentController.dispose();
-    super.dispose();
-  }
-}
-class RestoreNoteScreen extends StatelessWidget {
-  final List<String> deletedNotes;
-  final Function(String) onRestore;
-
-  const RestoreNoteScreen({
-    Key? key,
-    required this.deletedNotes,
-    required this.onRestore,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Restore Deleted Items"),
-      ),
-      body: ListView.builder(
-        itemCount: deletedNotes.length,
-        itemBuilder: (context, index) {
-          final noteParts = deletedNotes[index].split(':');
-          final title = noteParts[0];
-          final content = noteParts[1];
-
-          return ListTile(
-            title: Column(
+          const SizedBox(width: 20),
+          Expanded(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  title,
-                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  content,
-                  style: const TextStyle(fontSize: 16),
-                ),
+                for (NoteLine line in notes[index])
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                line.text1,
+                                style: line.style1,
+                              ),
+                              Text(
+                                line.text2,
+                                style: line.style2,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
               ],
             ),
-            trailing: IconButton(
-              icon: const Icon(Icons.restore),
-              onPressed: () {
-                onRestore(deletedNotes[index]);
-                Navigator.of(context).pop();
-              },
-            ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
 }
+
+class MyDivider extends StatelessWidget {
+  const MyDivider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      color: Colors.grey,
+      margin: const EdgeInsets.symmetric(vertical: 4),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
