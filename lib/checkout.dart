ca82b4149e8ed00210244006ed799f6fcbb4ae88
Yassa Ashraf <yassaashraf2468@gmail.com>
2024-03-18 03:06:05 +0200

    the Request

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -116,6 +116,8 @@ Future<bool> createShipmentRequest(BuildContext context, String pickUpLocation,
 
     if (response.statusCode == 200) {
       print('Shipment request created: ${response.body}');
+      print(response.body);
+      print('Status Code: ${response.statusCode}');
       displayToast('Shipment request created successfully');
       Navigator.pushReplacement(
         context,
@@ -124,6 +126,8 @@ Future<bool> createShipmentRequest(BuildContext context, String pickUpLocation,
       return true;
     } else {
       print('Failed to create shipment request: ${response.body}');
+      print(response.body);
+      print('Status Code: ${response.statusCode}');
       displayToast('Failed to create shipment request');
       return false;
     }
